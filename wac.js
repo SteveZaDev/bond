95033529a00a51f09faa4dea63d3d6d427d78508
SteveZaDev <118739097+SteveZaDev@users.noreply.github.com>
2022-12-20 10:58:34 -0500

    Add files via upload

--- wac.js
+++ wac.js
@@ -34,7 +34,7 @@ const villians = [
            "was the mistress of SPECTRE agent Emilio Largo. Bond discovers that Largo had killed Domino's brother, and thinks he can use that fact to get her on his side. They meet underwater as Bond untraps Domino's flipper from the coral, and Bond manages to convince her to plot against Largo", "met her end while dancing with Bond; a henchman tried to shoot Bond in the back, but he quickly spun round and the bullet killed Fiona instead.", "contacts Domino, and sees her connection to Emilio Largo, Bond and his team hunt for the bombs on board Largo’s yacht, the Disco Volante, and at his villa, Palmyra, but without success. After dispatching SPECTRE agent Fiona Volpe, Bond enlists the help of Domino by showing her proof that Largo killed her brother. "],
     pic:  ["url(./pics/largo.png)", "url(./pics/domino.jpg)", "url(./pics/fiona.jpg)", "url(./pics/bthunderball.jpg)"],    
     idx: 0,
-    audio: ["./auds/largo.m4a", "./auds/domino.m4a",, "./auds/fiona.m4a", "./auds/bthunderball.m4a"]
+    audio: ["./auds/largo.m4a", "./auds/domino.m4a", "./auds/fiona.m4a", "./auds/bthunderball.m4a"]
   },
   {
     name: ["Blofeld (Donald Pleasence)", "Aki", "Kissy Suzuki", "Helga Brandt", "You Only Live Twice"],
@@ -52,7 +52,7 @@ const villians = [
             "was the most important girl in Bond's life, becoming Mrs Tracy Bond by the end of the film she was killed with the help of Blofeld", "finds that Blofeld is brainwashing a group of women to act as his secret agents of biological warfare so that he can blackmail world powers. Bond infiltrates Piz Gloria disguised as Sir Hilary Bray of the College of Arms. When his real identity is discovered, Bond escapes and reunites with Tracy, but she is captured by Blofeld after being caught in an avalanche."],
     pic:  ["url(./pics/blofeld2.jpg)", "url(./pics/tracy.jpg)", "url(./pics/bsecret.jpg)"],   
     idx: 0,
-    audio: ["./auds/blofeld2.mp3", "./auds/tracy.m4a", "./auds/bsecret.m4a"]
+    audio: ["./auds/blo2.m4a", "./auds/tracy.m4a", "./auds/bsecret.m4a"]
   },
   {
     name: ["Blofeld (Charles Grey)", "Tiffany Case", "Plenty O'Toole", "Diamonds are Forever"],
@@ -69,7 +69,7 @@ const villians = [
           "was a psychic tarot card reader working for heroin baron Dr. Kananga. She predicted the movements of James Bond and other men who posed a threat to their operations", "was an inept and incompetent double agent, working for the CIA and for heroin baron Dr. Kananga.", "was an agent of the Italian Secret Service. She was asleep with Bond at his home when, just before 6am, the doorbell rang. Bond answered the door to find M, who announced an urgent mission.", "follows leads to New Orleans, then to San Monique, where he is aided by double agent Rosie Carver. After freeing Kananga’s girlfriend, Solitaire, a seer who foretells the future with Tarot cards, Bond discovers that Kananga and Mr. Big are one and the same. "],
     pic:  ["url(./pics/mrbig.jpg)", "url(./pics/sol.jpg)", "url(./pics/rosie.jpg)", "url(./pics/caruso.jpg)", "url(./pics/blive.jpg)"],   
     idx: 0,
-    audio: ["./auds/mrbig.m4a", "./auds/sol.mp3", "./auds/rosie.mp3", "./auds/caruso.mp3", "./auds/blive.m4a"]
+    audio: ["./auds/mrbig.m4a", "./auds/sol.m4a", "./auds/rosie.m4a", "./auds/caruso.m4a", "./auds/blive.m4a"]
   },
   {
     name: ["Francisco Scaramanga", "Mary Goodnight", "Andrea Anders", "The Man with the Golden Gun"],
@@ -78,7 +78,7 @@ const villians = [
           "Mary Goodnight was Bond's assistant in Hong Kong, and previously his secretary in London. She helps Bond to locate Miss Anders, the girlfriend of hitman Scaramanga.", "was the girlfriend of Francisco Scaramanga, the $1 million a shot assassin, the man with the golden gun. She sent a golden bullet to MI6 with 007 engraved on the side, so that it would be thought that Scaramanga had Bond on his hit list.", "receives a gold bullet inscribed with “007”, signifying he has been targeted by high-class professional assassin Francisco Scaramanga known as “The Man with the Golden Gun”."],
     pic:  ["url(./pics/scar.jpg)", "url(./pics/mary.jpg)", "url(./pics/andrea.jpg)", "url(./pics/bgolden.jpg)"],
     idx: 0,
-    audio: ["./auds/scar.m4a", "./auds/mary.mp3", "./auds/andrea.mp3", "./auds/bgolden.m4a"]
+    audio: ["./auds/scar.m4a", "./auds/mary.m4a", "./auds/andrea.m4a", "./auds/bgolden.m4a"]
   },
   {
     name: ["Karl Stromberg", "Anya Amasova", "Naomi", "Felicca", "The Spy Who Loved Me"],
@@ -87,43 +87,43 @@ const villians = [
           "code name 'Triple X', was a KGB agent working under General Gogol. She had the same mission as Bond, to retrieve stolen microfilms for a submarine tracking system.", "was an aid for Karl Stromberg, and escorted Bond and Anya Amasova to Stromberg's oceanic citadel, the Atlantis. After Bond's meeting ended, Stromberg instructed henchman Jaws to kill the two spies.", "took a liking to Bond, and when she saw Sandor about to shoot him, she spun round and took the bullet herself.", " travels to Egypt, where illicit microfilm plans for a submarine tracking system are being offered for sale. In Cairo he meets KGB agent Major Anya Amasova, who is on the same mission. After their contact is murdered, they fight Jaws, a steel-toothed villain in the pay of industrialist Karl Stromberg."],
     pic:  ["url(./pics/strom.jpg)", "url(./pics/anya.jpg)", "url(./pics/naomi.jpg)", "url(./pics/felicca.jpg)", "url(./pics/bspy.jpg)"],
     idx: 0,
-    audio: ["./auds/strom.mp3", "./auds/anya.mp3", "./auds/naomi.mp3", "./auds/felicca.mp3", "./auds/bspy.m4a"]
+    audio: ["./auds/strom.m4a", "./auds/anya.m4a", "./auds/naomi.m4a", "./auds/felicca.m4a", "./auds/bspy.m4a"]
   },
   {
-    name: ["Hugo Drax", "Corinne Dufour", "Moonraker"],
+    name: ["Hugo Drax", "Corinne Dufour", "Dr. Holly Goodhead", "Moonraker"],
     movie: "Moonraker",
     fate: ["was shot with poison dart gun and ejected into outer space, by Bond. ",
-            "was Hugo Drax's personal assistant and pilot. She flew Bond over Drax's estate, giving him a guided tour of the Moonraker complex and flying him to meet Drax. After spending the night with Bond, Corinne revealed the location of Drax's safe, where Bond photographed secret documents. Drax discovered this, and Corinne was killed by Drax's vicious pack of dogs.", "follows clues that lead him first to Venice, where he discovers Drax’s laboratory manufacturing a highly toxic nerve gas, and then to Rio, where he teams up with CIA agent and astrophysicist Holly Goodhead."],
-    pic:  ["url(./pics/drax.jpg)", "url(./pics/corinne.jpg)", "url(./pics/bmoon.jpg)"],
+            "was Hugo Drax's personal assistant and pilot. She flew Bond over Drax's estate, giving him a guided tour of the Moonraker complex and flying him to meet Drax. After spending the night with Bond, Corinne revealed the location of Drax's safe, where Bond photographed secret documents. Drax discovered this, and Corinne was killed by Drax's vicious pack of dogs.", "was a CIA agent working undercover as an astronaut for the Hugo Drax organization. After spending the night together with Bond, the two met up at the top of Sugarloaf mountain in Rio. On the cable car ride down, Jaws tried to kill them, and Holly's cover was blown.", "follows clues that lead him first to Venice, where he discovers Drax’s laboratory manufacturing a highly toxic nerve gas, and then to Rio, where he teams up with CIA agent and astrophysicist Holly Goodhead."],
+    pic:  ["url(./pics/drax.jpg)", "url(./pics/corinne.jpg)", "url(./pics/holly.jpg)", "url(./pics/bmoon.jpg)"],
     idx: 0,
-    audio: ["./auds/drax.m4a", "./auds/corinne.mp3", "./auds/bmoon.m4a"]
+    audio: ["./auds/drax.m4a", "./auds/corinne.m4a", "./auds/holly.m4a", "./auds/bmoon.m4a"]
   },
   {
-    name: ["Aristotle Kristatos", "Melina Havelock", "For Your Eyes Only"],
+    name: ["Aristotle Kristatos", "Melina Havelock",  "Bibi Dahl", "For Your Eyes Only"],
     movie: "For Your Eyes Only",
     fate: ["was killed with a knife thrown by Milos Columbo. ",
-            "was the daughter of two marine explorers who did salvage work for the British Secret Service. After her parents were assassinated by henchman Hector Gonzales, Melina found the hitman and killed him with a crossbow.", "is ordered to retrieve the Automatic Targeting Attack Communicator (ATAC), housed on a sunken spy ship, the St. Georges. When Sir Timothy Havelock, a marine archaeologist secretly helping the British to locate the ship, is murdered along with his wife, Bond is dispatched to Spain to find out who hired the hitman, Gonzales, but before he can find out, Gonzales is killed by Havelock’s daughter, Melina."],
-    pic:  ["url(./pics/ari.jpg)", "url(./pics/melina.jpg)", "url(./pics/beyes.jpg)"],
+            "was the daughter of two marine explorers who did salvage work for the British Secret Service. After her parents were assassinated by henchman Hector Gonzales, Melina found the hitman and killed him with a crossbow.", "was a young ice skater sponsored by double agent Kristatos. Bibi was very attracted to Bond, however he turned her down on every occasion due to her young age.", "is ordered to retrieve the Automatic Targeting Attack Communicator (ATAC), housed on a sunken spy ship, the St. Georges. When Sir Timothy Havelock, a marine archaeologist secretly helping the British to locate the ship, is murdered along with his wife, Bond is dispatched to Spain to find out who hired the hitman, Gonzales, but before he can find out, Gonzales is killed by Havelock’s daughter, Melina."],
+    pic:  ["url(./pics/ari.jpg)", "url(./pics/melina.jpg)", "url(./pics/bibi.jpg)", "url(./pics/beyes.jpg)"],
     idx: 0,
-    audio: ["./auds/ari.m4a", "./auds/melina.mp3", "./auds/beyes.m4a"]
+    audio: ["./auds/ari.m4a", "./auds/melina.m4a", "./auds/bibi.m4a", "./auds/beyes.m4a"]
   },
   {
-    name: ["Kamal Khan", "Octopussy", "Octopussy"],
+    name: ["Kamal Khan", "Octopussy", "Magda", "Octopussy"],
     movie: "Octopussy",
     fate: ["dies when his plane crashes into a mountain.",
-          "ran an all-women floating island, the girls of which were acrobats for a circus that was a front for a jewellery smuggling operation. She was a partner in crime with Kamal Khan, who stole the jewellery for himself and tried to kill Octopussy.", "swaps a real Faberge egg with a fake, and drives up the bidding against exiled Afghan prince Kamal Khan, who ultimately wins the auction. Bond follows Khan back to his palace in Rajasthan, India, where he discovers that Khan is working with Orlov, a renegade Soviet general, and is using Octopussy’s circus troupe to smuggle Soviet treasures into the West."],
-    pic:  ["url(./pics/khan.jpg)", "url(./pics/octo.jpg)", "url(./pics/bocto.jpg)"],
+          "ran an all-women floating island, the girls of which were acrobats for a circus that was a front for a jewellery smuggling operation. She was a partner in crime with Kamal Khan, who stole the jewellery for himself and tried to kill Octopussy.", "was the right-hand woman of Octopussy, and assisted her smuggling partner Kamal Khan on many occasions throughout the film. After attending an auction in which Bond stole a Fabegé egg, Magda was sent to sleep with Bond so that she could steal it back.","swaps a real Faberge egg with a fake, and drives up the bidding against exiled Afghan prince Kamal Khan, who ultimately wins the auction. Bond follows Khan back to his palace in Rajasthan, India, where he discovers that Khan is working with Orlov, a renegade Soviet general, and is using Octopussy’s circus troupe to smuggle Soviet treasures into the West."],
+    pic:  ["url(./pics/khan.jpg)", "url(./pics/octo.jpg)", "url(./pics/magda.jpg)", "url(./pics/bocto.jpg)"],
     idx: 0,
-    audio: ["./auds/khan.mp3", "./auds/octo.mp3", "./auds/bocto.m4a"]
+    audio: ["./auds/kamal.m4a", "./auds/octo.m4a", "./auds/magda.m4a", "./auds/bocto.m4a"]
   },
   {
-    name: ["Max Zorin", "Stacey Sutton", "MayDay", "A View to a Kill"],
+    name: ["Max Zorin", "Stacey Sutton", "MayDay", "Pola Ivanova", "A View to a Kill"],
     movie: "A View to a Kill",
     fate: ["falls to his death from the top of the Golden Gate Bridge after a fight with Bond.",
-          "inherited the Sutton Oil company, which megalomaniac Max Zorin tried to take over. Zorin offered Sutton $5 million for her shares in the company, which she refused.", "maydays fate", "recovers a microchip from the body of 003 in Siberia which is a copy of one that is impervious to the magnetic pulse of a nuclear blast. It is made by a company recently acquired by Anglo-French combine Zorin Industries, so Bond is assigned to investigate Max Zorin."],
-    pic:  ["url(./pics/zorin.jpg)", "url(./pics/stacey.jpg)", "url(./pics/mayday.jpg)","url(./pics/bview.png)"],
+          "inherited the Sutton Oil company, which megalomaniac Max Zorin tried to take over. Zorin offered Sutton $5 million for her shares in the company, which she refused.", "maydays fate", "was a KGB agent working for General Gogol. She recorded incriminating evidence of Max Zorin, and escaped with a tape of it. Bond had followed her, and a struggle ensued, until they both realized that they knew one another from a prior mission.", "recovers a microchip from the body of 003 in Siberia which is a copy of one that is impervious to the magnetic pulse of a nuclear blast. It is made by a company recently acquired by Anglo-French combine Zorin Industries, so Bond is assigned to investigate Max Zorin."],
+    pic:  ["url(./pics/zorin.jpg)", "url(./pics/stacey.jpg)", "url(./pics/mayday.jpg)", "url(./pics/pola.jpg)", "url(./pics/bview.png)"],
     idx: 0,
-    audio: ["./auds/zorin.m4a", "./auds/stacey.mp3", "./auds/mayday.mp3", "./auds/bview.m4a"]
+    audio: ["./auds/zorin.m4a", "./auds/stacey.m4a", "./auds/mayday.m4a", "./auds/pola.m4a", "./auds/bview.m4a"]
   },
   {
     name: ["General Georgi Koskov", "Kara Milovy", "The Living Daylights"],
@@ -132,7 +132,7 @@ const villians = [
           "was the girlfriend of Georgi Koskov, a traitor playing both sides in the cold war. Koskov pretended to defect to the West, and got Milovy to shoot blanks at him to make it look more real.", "is sent to Czechoslovakia to support the defection of a Russian Army Officer, Koskov. He becomes suspicious when the sniper he has to assassinate appears to be a glamorous cellist, Kara Milovy. "],
     pic:  ["url(./pics/georgi.jpg)", "url(./pics/kara.jpg)", "url(./pics/bliving.jpg)"],
     idx: 0,
-    audio: ["./auds/georgi.mp3", "./auds/kara.mp3", "./auds/bliving.m4a"]
+    audio: ["./auds/georgi.m4a", "./auds/kara.m4a", "./auds/bliving.m4a"]
   }
 ];
 
